f2603f7eb66b4c9288905c19022d593103f3bc63
dcbarros <dcbarros1@gmail.com>
2024-01-25 14:50:34 -0300

    refactor: remoção de imports desnecessários

--- Agenda-Telefonica/src/service/ContatosService.java
+++ Agenda-Telefonica/src/service/ContatosService.java
@@ -8,9 +8,8 @@ import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
-import java.util.Scanner;
+
 
 import models.Contatos;
 import models.Telefone;
